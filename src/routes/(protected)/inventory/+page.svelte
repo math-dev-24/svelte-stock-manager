dd9345df495fee6575e8d265b1b571e3f2107e77
Mathieu <mathieu.busse@live.fr>
2025-07-04 17:56:28 +0200

    Update inventory page to display selected company name and enhance layout
    - Added TypeScript script to retrieve company data from props.
    - Updated the heading to reflect the selected company's name.
    - Improved layout structure with additional styling for better presentation.

--- src/routes/(protected)/inventory/+page.svelte
+++ src/routes/(protected)/inventory/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+    const {data} = $props();
+</script>
+<h1>Inventory of {data.selectedCompany?.name}</h1>
 
-<h1>Inventory</h1>
+<div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-2">
+        <h2 class="text-lg font-bold">Inventory</h2>
+    </div>
+</div>
